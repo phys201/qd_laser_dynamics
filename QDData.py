4ca32329a9664fe36f31ec9a0fe00b479aefae58
dfernandez97 <dfernandez@g.harvard.edu>
2021-04-12 16:39:04 -0400

    Creation of class to set up an object to hold data and metadata.

---
+++ QDData.py
@@ -0,0 +1,38 @@
+
+    
+"""
+This submodule contains the definition of the QDData class,
+which holds Quantum Dot readout data and metadata.
+"""
+
+import os
+import pandas as pd
+
+
+class QDData:
+    """A class that puts data into a pandas data frame object.
+    
+    Parameters:
+        filepath: path to data file (specified as a string)  
+    """
+    def __init__(self, filepath):        
+        """
+        The constructor for an QDData object
+        Parameters:
+            filepath: the path to the data file, relative to the working directory (string)
+        Outputs:
+            self: an QDData object, which contains a Pandas
+                DataFrame holding the data loaded from the file (QDData)
+        """
+        self.filename = filepath
+        self._dataframe = pd.read_csv(filepath, encoding='utf-8', delimiter="\t", header=None)
+        
+    def get_df(self):
+        """
+        Returns data frame object that contains data from file.
+        Parameters:
+            none
+        Outputs:
+            dataframe: a Pandas DataFrame holding the data (DataFrame)
+        """
+        return self._dataframe
