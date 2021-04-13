47d3ff5f7817dbc7fb2d4cdedc52a62760b03036
elizabeth7831 <82106021+elizabeth7831@users.noreply.github.com>
2021-04-12 21:40:38 -0400

    Update test_io.py

--- example/tests/test_io.py
+++ example/tests/test_io.py
@@ -4,8 +4,9 @@ import pandas as  pd
 
 class TestIo(TestCase):
     def test_data_io(self):
-        data = load_data(get_example_data_file_path('example_data.txt'))
-        assert data.x[0] == 1
+        data = load_data(get_example_data_file_path('simulated_data_csv.csv'))
+        assert data.x[0] == 270526
 #
-# if __name__ == '__main__':
-#     unittest.main()
+if __name__ == '__main__':
+    unittest.main()
+
